d44c6a5e92e0627b227b8fd1cb16365bf8024e6c
railombardi <raidslombardi@gmail.com>
2023-03-02 01:51:40 -0300

    ✨ initial structure and nav

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,38 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view />
+    <div class="nav">
+      <SvgIcons icon="kotas" :width="80" :height="24" />
+    </div>
+    <div class="container">
+      <router-view />
+    </div>
   </div>
 </template>
+<script>
+import SvgIcons from "./components/SvgIcons.vue";
 
-<style lang="scss">
+export default {
+  components: { SvgIcons },
+};
+</script>
+<style lang="scss" scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+  font-family: $font-mulish;
+  overflow: hidden;
+  height: 100vh;
+  .nav {
+    background: white;
+    padding: 12px 0 8px 16px;
+    box-shadow: 0px 1px 1px #e1e1e1;
+    @include mobile() {
+      display: flex;
+      justify-content: center;
     }
+  }
+  .container {
+    width: 100%;
+    height: 100%;
+    margin: 0 auto;
   }
 }
 </style>
